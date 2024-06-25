e52a5b7d78072c616e01e56043309375822e9e72
Luminaxis <luminaxistechnologies@gmail.com>
2024-06-25 15:01:52 +0500

    updated index.js

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ import cors from 'cors';
 import connectDB from './config/db.js';
 import userRoutes from './routes/userRoutes.js';
 import videoRoutes from './routes/videoRoutes.js';
-import path from 'path';
 
 dotenv.config();
 
@@ -28,26 +27,10 @@ app.use('/api/users', userRoutes);
 app.use('/api/videos', videoRoutes);
 
 // Serve static files from the 'uploads' directory
+const path = require('path');
 const __dirname = path.resolve();
 const uploadsPath = path.join(__dirname, 'uploads');
 app.use('/uploads', express.static(uploadsPath));
-
-// Serve frontend static files in production
-if (process.env.NODE_ENV === 'production') {
-  // Serve static files from the frontend 'dist' directory
-  const frontendPath = path.join(__dirname, '..', 'frontend', 'dist');
-  app.use(express.static(frontendPath));
-
-  // Serve 'index.html' for any other routes
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(frontendPath, 'index.html'));
-  });
-} else {
-  // Default route for non-production environment
-  app.get('/', (req, res) => {
-    res.send('API is running....');
-  });
-}
 
 // Error handling for 404 Not Found
 app.use((req, res, next) => {
@@ -63,5 +46,5 @@ app.use((err, req, res, next) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+  console.log(`Server running in ${process.env.NODE_ENV || 'development'} mode on port ${PORT}`);
 });
